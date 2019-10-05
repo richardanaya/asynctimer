43d0ca36813a388f574ace1c022a69a1c01f3f1b
richard <richard>
2019-10-05 10:26:49 -0700

    adding no op waker for now

--- src/executor.rs
+++ src/executor.rs
@@ -4,6 +4,7 @@ use std::{
         mpsc::{sync_channel, Receiver, SyncSender},
         Arc, Mutex,
     },
+    task::{RawWaker, RawWakerVTable,Waker,Context,Poll}
 };
 
 struct Executor {
@@ -19,6 +20,25 @@ struct Task {
     future: Mutex<Option<Box<dyn Future<Output = ()>>>>,
     task_sender: SyncSender<Arc<Task>>,
 }
+
+unsafe fn noop_clone(_data: *const ()) -> RawWaker {
+    noop_raw_waker()
+}
+
+fn noop_raw_waker() -> RawWaker {
+    RawWaker::new(core::ptr::null(), &NOOP_WAKER_VTABLE)
+}
+
+unsafe fn noop(_data: *const ()) {}
+
+const NOOP_WAKER_VTABLE: RawWakerVTable = RawWakerVTable::new(noop_clone, noop, noop, noop);
+    
+impl Task {
+    fn get_waker(&self) -> Waker {
+        Waker::from_raw(noop_raw_waker())
+    }
+}
+
 
 fn new_executor_and_spawner() -> (Executor, Spawner) {
     const MAX_QUEUED_TASKS: usize = 10_000;
@@ -42,10 +62,10 @@ impl Executor {
         while let Ok(task) = self.ready_queue.recv() {
             let mut future_slot = task.future.lock().unwrap();
             if let Some(mut future) = future_slot.take() {
-                /*let context = &mut Context::from_waker((&task);
+                let context = &mut Context::from_waker(&task.get_waker());
                 if let Poll::Pending = future.as_mut().poll(context) {
                     *future_slot = Some(future);
-                }*/
+                }
             }
         }
     }
